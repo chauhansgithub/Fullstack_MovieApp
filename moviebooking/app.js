129d89d4f05a0c6225d7508507976e855991f5c2
chauhan pruthvi raj <cpruthvi.raj.cse13@iitbhu.ac.in>
2021-07-22 18:21:27 +0530

    Checkpoint 6

---
+++ moviebooking/app.js
@@ -0,0 +1,42 @@
+//external imports
+const express = require("express");
+const cors = require("cors");
+
+const db = require("./models");
+const genreRouter = require("./routes/genre.routes");
+const artistRouter = require("./routes/artist.routes");
+const movieRouter = require("./routes/movie.routes");
+//constants
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Upgrad Movie booking application development.",
+  });
+});
+
+app.use("/api/genres", genreRouter);
+app.use("/api/artists", artistRouter);
+app.use("/api/movies", movieRouter);
+
+module.exports = app;
--- moviebooking/server.js
+++ moviebooking/server.js
@@ -1,34 +1,36 @@
 const http = require("http");
-const db = require("./models");
+//const db = require("./models");
+const app = require("./app");
 
-const PORT = 9000;
+const PORT = process.env.PORT || 3000;
 
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+const server = http.createServer(app);
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch(err => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
 
-  
-const ROUTE_RESPONSE_MAP = {
-  "/movies": "All Movies Data in JSON format from Mongo DB",
-  "/genres": "All Genres Data in JSON format from Mongo DB",
-  "/artists": "All Artists Data in JSON format from Mongo DB",
-};
 
-const server = http.createServer((req, res) => {
-  if (ROUTE_RESPONSE_MAP[req.url]) {
-    res.end(ROUTE_RESPONSE_MAP[req.url]);
-  }
-});
+// const ROUTE_RESPONSE_MAP = {
+//   "/movies": "All Movies Data in JSON format from Mongo DB",
+//   "/genres": "All Genres Data in JSON format from Mongo DB",
+//   "/artists": "All Artists Data in JSON format from Mongo DB",
+// };
+
+// const server = http.createServer((req, res) => {
+//   if (ROUTE_RESPONSE_MAP[req.url]) {
+//     res.end(ROUTE_RESPONSE_MAP[req.url]);
+//   }
+// });
 
 server.listen(PORT, () => {
-  console.log(`server listening on port 9000`);
+  console.log(`server listening on port ${PORT}`);
 });
