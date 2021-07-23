54977a73e95905f17609a6e687b370e63d3934ad
chauhan pruthvi raj <cpruthvi.raj.cse13@iitbhu.ac.in>
2021-07-23 13:14:34 +0530

    Checkpoint 9

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -2,34 +2,10 @@ const http = require("http");
 //const db = require("./models");
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8085;
 
 const server = http.createServer(app);
-// db.mongoose
-//   .connect(db.url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to the database!");
-//   })
-//   .catch(err => {
-//     console.log("Cannot connect to the database!", err);
-//     process.exit();
-//   });
 
-
-// const ROUTE_RESPONSE_MAP = {
-//   "/movies": "All Movies Data in JSON format from Mongo DB",
-//   "/genres": "All Genres Data in JSON format from Mongo DB",
-//   "/artists": "All Artists Data in JSON format from Mongo DB",
-// };
-
-// const server = http.createServer((req, res) => {
-//   if (ROUTE_RESPONSE_MAP[req.url]) {
-//     res.end(ROUTE_RESPONSE_MAP[req.url]);
-//   }
-// });
 
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
